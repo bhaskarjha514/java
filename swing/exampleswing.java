1c47bdd07a6da78241b8dd9130d8fe840ff35d9f
bhaskarjha514 <bhaskarjha514>
2019-12-04 23:31:05 +0530

    first

---
+++ swing/exampleswing.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import javax.swing.JFrame;
+public class exampleswing{
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Swing");
+        jf.setVisible(true);
+        jf.setSize(300,300);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
